8d8b584c7c702ab2e1938a34a9c5934cd70384f9
Lewis Howles <lewishowles@users.noreply.github.com>
2026-06-18 15:53:14 +0100

    fix(config): respect NO_COLOR and non-TTY contexts for colour output

--- aliases.config.zsh
+++ aliases.config.zsh
@@ -1,13 +1,18 @@
-# Colours for CLI output.
-BLACK=$'\033[0;30m'
-RED=$'\033[0;31m'
-GREEN=$'\033[0;32m'
-YELLOW=$'\033[0;33m'
-BLUE=$'\033[0;34m'
-PURPLE=$'\033[0;35m'
-CYAN=$'\033[0;36m'
-WHITE=$'\033[0;37m'
-RESET_COLOUR=$'\033[0m'
+# Colours for CLI output. Disabled when NO_COLOR is set or stdout is not a TTY.
+# https://no-color.org
+if [[ -t 1 ]] && [[ -z "${NO_COLOR:-}" ]]; then
+	BLACK=$'\033[0;30m'
+	RED=$'\033[0;31m'
+	GREEN=$'\033[0;32m'
+	YELLOW=$'\033[0;33m'
+	BLUE=$'\033[0;34m'
+	PURPLE=$'\033[0;35m'
+	CYAN=$'\033[0;36m'
+	WHITE=$'\033[0;37m'
+	RESET_COLOUR=$'\033[0m'
+else
+	BLACK='' RED='' GREEN='' YELLOW='' BLUE='' PURPLE='' CYAN='' WHITE='' RESET_COLOUR=''
+fi
 
 # @desc  Open .zshrc in VS Code
 # @cat   config
